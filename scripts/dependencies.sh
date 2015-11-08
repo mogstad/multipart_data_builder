3f44e6c3fb6ca63f950fee3ac1ceb19cc65144ca
Bjarne Mogstad <bjarne@metalabdesign.com>
2015-11-08 15:19:38 +0100

    Update dependencies

--- scripts/dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl -O -L https://github.com/Carthage/Carthage/releases/download/0.9.2/Carthage.pkg
-sudo installer -pkg Carthage.pkg -target /
-
-carthage bootstrap --use-ssh --platform ios
