1c47118ff4b8a1f3b092675ef98262b2749e14b4
Malcolm Jarvis <malcolm@metalab.co>
2016-08-25 14:24:42 -0700

    Fix upload of edited images / videos
    PHAssetResource contains only the original file, and as such will not include any edits to the file.
    Instead, we need to use `PHImageManager.requestExportSessionForVideo` and `PHImageManager.requestImageDataForAsset` to ensure we get the current version of the image.

--- sources/asset_data_source.swift
+++ sources/asset_data_source.swift
@@ -2,47 +2,94 @@ import Foundation
 import MobileCoreServices
 import Photos
 
+func contentTypeForUTI(UTI: String) -> String {
+  if let mimeType = UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassMIMEType) {
+    return mimeType.takeRetainedValue() as String
+  }
+  return "application/octet-stream"
+}
+
 class AssetDataSource: ChunkDataSource {
 
+  enum Error: ErrorType {
+    case NoExportSession
+    case ExportSessionFailed
+    case NoImageData
+    case NoImageUTI
+    case NoResourceAsset
+    case UnknownMediaType
+  }
+
   let name: String
-  let asset: PHAssetResource
-  let contentType: String
+  let asset: PHAsset
   var request: PHAssetResourceDataRequestID?
 
-  init(name: String, contentType: String, asset: PHAssetResource) {
+  init(name: String, asset: PHAsset) {
     self.asset = asset
-    self.contentType = contentType
     self.name = name
   }
 
   func write(outputStream: NSOutputStream, completeHandler: AssetDataSourceCompleteHandler) {
-    do {
-      try self.writePrologue(self.asset, outputStream: outputStream)
-    } catch {
-      return completeHandler(error)
-    }
+    let manager = PHImageManager.defaultManager()
+    switch self.asset.mediaType {
+    case .Video:
 
-    self.request = PHAssetResourceManager.defaultManager().requestDataForAssetResource(self.asset,
-      options: nil,
-      dataReceivedHandler: { data in
-        do {
-          try self.writeData(data, outputStream: outputStream)
-        } catch {
-          completeHandler(error)
+      let options = PHVideoRequestOptions()
+      options.networkAccessAllowed = true
+      manager.requestExportSessionForVideo(self.asset, options: options, exportPreset: AVAssetExportPresetHighestQuality) { (exportSession, info) in
+        guard let exportSession = exportSession else {
+          return completeHandler(Error.NoExportSession)
         }
-      },
-      completionHandler: { error in
-        if let error = error {
-          completeHandler(error)
-        } else {
+
+        let fileType = AVFileTypeQuickTimeMovie
+        exportSession.outputFileType = fileType
+        let date = NSDate().timeIntervalSince1970
+        let outputURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent("temp-\(date).mov")
+        if NSFileManager.defaultManager().fileExistsAtPath(outputURL.path!) {
+            let _ = try? NSFileManager.defaultManager().removeItemAtURL(outputURL)
+        }
+        exportSession.outputURL = outputURL
+        exportSession.exportAsynchronouslyWithCompletionHandler({ 
+
+          guard case AVAssetExportSessionStatus.Completed = exportSession.status else {
+            return completeHandler(Error.ExportSessionFailed)
+          }
+
+          let inputStream = NSInputStream(URL: outputURL)!
           do {
+            try self.writePrologue(self.asset, contentType: contentTypeForUTI(fileType), outputStream: outputStream)
+            try self.writeStream(inputStream, outputStream: outputStream)
             try self.writeData(MutlipartFormCRLFData, outputStream: outputStream)
-            completeHandler(error)
+            completeHandler(nil)
           } catch {
             completeHandler(error)
           }
+        })
+      }
+    case .Image:
+      let options = PHImageRequestOptions()
+      options.networkAccessAllowed = true
+      self.request = manager.requestImageDataForAsset(self.asset, options: options) { (data, uti, orientation, info) in
+        guard let data = data else {
+          return completeHandler(Error.NoImageData)
         }
-    })
+
+        guard let uti = uti else {
+          return completeHandler(Error.NoImageUTI)
+        }
+
+        do {
+          try self.writePrologue(self.asset, contentType: contentTypeForUTI(uti), outputStream: outputStream)
+          try self.writeData(data, outputStream: outputStream)
+          try self.writeData(MutlipartFormCRLFData, outputStream: outputStream)
+          completeHandler(nil)
+        } catch {
+          completeHandler(error)
+        }
+      }
+    default:
+      completeHandler(Error.UnknownMediaType)
+    }
   }
 
   func cancel() {
@@ -51,10 +98,12 @@ class AssetDataSource: ChunkDataSource {
     }
   }
 
-  private func writePrologue(resource: PHAssetResource, outputStream: NSOutputStream) throws {
+  private func writePrologue(asset: PHAsset, contentType: String, outputStream: NSOutputStream) throws {
+    let resources = PHAssetResource.assetResourcesForAsset(asset)
+    guard let resource = primaryAssetResource(resources) else { throw Error.NoResourceAsset }
     let contentDisposition = "Content-Disposition: form-data; name=\"\(encode(self.name))\"; filename=\"\(encode(resource.originalFilename))\""
 
-    let contentTypeHeader = "Content-Type: \(self.contentType)"
+    let contentTypeHeader = "Content-Type: \(contentType)"
     let prologue = merge([
       toData(contentDisposition),
       MutlipartFormCRLFData,
@@ -72,4 +121,9 @@ class AssetDataSource: ChunkDataSource {
     try writeBuffer(buffer, toOutputStream: outputStream)
   }
 
+  private func writeStream(inputStream: NSInputStream, outputStream: NSOutputStream) throws {
+    try readStream(inputStream) { buffer in
+      try writeBuffer(buffer, toOutputStream: outputStream)
+    }
+  }
 }
--- sources/multipart_asset_field.swift
+++ sources/multipart_asset_field.swift
@@ -1,14 +1,7 @@
 import Photos
 import MobileCoreServices
 
-func contentTypeForResource(resource: PHAssetResource) -> String {
-  if let mimeType = UTTypeCopyPreferredTagWithClass(resource.uniformTypeIdentifier, kUTTagClassMIMEType) {
-    return mimeType.takeRetainedValue() as String
-  }
-  return "application/octet-stream"
-}
-
-func primaryAsset(resources: [PHAssetResource]) -> PHAssetResource? {
+func primaryAssetResource(resources: [PHAssetResource]) -> PHAssetResource? {
   let index = resources.indexOf { resource -> Bool in
     let primaryType = resource.type == .Video || resource.type == .Photo || resource.type == .Audio
     return primaryType
@@ -19,23 +12,29 @@ func primaryAsset(resources: [PHAssetResource]) -> PHAssetResource? {
 
 public struct MultipartAssetField: MultipartField {
 
-  public let asset: PHAssetResource
-  public let name: String
+  public let asset: PHAsset
   public let contentType: String
+  public let name: String
 
   public init?(name: String, asset: PHAsset) {
-    let assets = PHAssetResource.assetResourcesForAsset(asset)
-    guard let asset = primaryAsset(assets) else { return nil }
-
     self.name = name
     self.asset = asset
-    self.contentType = contentTypeForResource(asset)
+
+    switch asset.mediaType {
+    case .Video:
+      self.contentType = contentTypeForUTI(AVFileTypeQuickTimeMovie)
+    case .Image:
+      let resources = PHAssetResource.assetResourcesForAsset(asset)
+      guard let resource = primaryAssetResource(resources) else { return nil }
+      self.contentType = contentTypeForUTI(resource.uniformTypeIdentifier)
+    default:
+      return nil
+    }
   }
 
   public func dataSource() -> ChunkDataSource {
     return AssetDataSource(
       name: self.name,
-      contentType: self.contentType,
       asset: self.asset)
   }
 
